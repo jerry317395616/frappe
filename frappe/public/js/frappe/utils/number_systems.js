082858fd84a937cc300d8657b956699a619d6c1d
jerry317395616 <317395616@qq.com>
2024-07-26 23:53:13 +0800

    2024-07-26 更改数字系统

--- frappe/public/js/frappe/utils/number_systems.js
+++ frappe/public/js/frappe/utils/number_systems.js
@@ -1,20 +1,36 @@
 export default {
 	default: [
+		// {
+		// 	divisor: 1.0e12,
+		// 	symbol: __("T", null, "Number system"),
+		// },
+		// {
+		// 	divisor: 1.0e9,
+		// 	symbol: __("B", null, "Number system"),
+		// },
+		// {
+		// 	divisor: 1.0e6,
+		// 	symbol: __("M", null, "Number system"),
+		// },
+		// {
+		// 	divisor: 1.0e3,
+		// 	symbol: __("K", null, "Number system"),
+		// },
 		{
 			divisor: 1.0e12,
-			symbol: __("T", null, "Number system"),
+			symbol: __("兆", null, "Number system"), // 10^12 is read as 1 兆
 		},
 		{
-			divisor: 1.0e9,
-			symbol: __("B", null, "Number system"),
+			divisor: 1.0e8,
+			symbol: __("亿", null, "Number system"), // 10^8 is read as 1 亿
 		},
 		{
-			divisor: 1.0e6,
-			symbol: __("M", null, "Number system"),
+			divisor: 1.0e4,
+			symbol: __("万", null, "Number system"), // 10^4 is read as 1 万
 		},
 		{
 			divisor: 1.0e3,
-			symbol: __("K", null, "Number system"),
+			symbol: __("千", null, "Number system"), // 10^3 is read as 1 千
 		},
 	],
 	indian: [
